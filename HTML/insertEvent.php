4613df57ab8af5fc8aba4d71becdcc4251e87369
Christian Dudukovich <christian.dudukovich@yahoo.com>
2018-11-07 16:08:08 -0500

    Finished new event creation and added google maps

--- HTML/insertEvent.php
+++ HTML/insertEvent.php
@@ -15,10 +15,13 @@ if (!$con)
 
 mysqli_select_db($con, "events");
 
-
-$sql="INSERT INTO events (name, description, category, type, phone, email, date_of_event, event_time)
+$datetime = $_POST['date'] . ' ' . $_POST['time'];
+$datetime = mysqli_real_escape_string($con, $datetime);
+$datetime = strtotime($datetime);
+$datetime = date('Y-m-d H:i:s',$datetime);
+$sql="INSERT INTO events (name, description, category, type, phone, email, lat, log, event_datetime)
 VALUES
-('$_POST[name]', '$_POST[description]', '$_POST[category]', '$_POST[type]', '$_POST[phone]', '$_POST[email]', '$_POST[date]', '$_POST[time]')";
+('$_POST[name]', '$_POST[comment]', '$_POST[category]', '$_POST[type]', '$_POST[phone]', '$_POST[email]', '$_POST[lat]', '$_POST[long]', '$datetime')";
 
 mysqli_query($con, $sql) or trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error($con), E_USER_ERROR);
 
