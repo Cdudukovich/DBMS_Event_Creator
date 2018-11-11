edf1d26f71a4da585b22f3993005a824ef42ad8b
John Sermarini <JohnSermarini@knights.ucf.edu>
2018-11-11 13:44:13 -0500

    add univeristy and edit comment added

---
+++ HTML/editComment.php
@@ -0,0 +1,108 @@
+<?php
+// Always start this first
+session_start();
+$db_host = 'localhost'; // Server Name
+$db_user = 'root'; // Username
+$db_pass = ''; // Password
+$db_name = 'event_creator'; // Database Name
+
+$_SESSION["LoggedIn"] = true;
+$_SESSION["Level"] = "admin";
+
+// for testing - delete later
+$_SESSION["commentID"] = 12344;
+
+
+$id = $_SESSION["commentID"]; // Need to get from previous page
+
+$query = "SELECT c_id, id, comment_text 
+            FROM users 
+            WHERE c_id = '$id'";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+if (!$conn) {
+    die ('Failed to connect to MySQL: ' . mysqli_connect_error());  
+}
+
+$result = mysqli_query($conn, $query);
+$count = mysqli_num_rows($result); 
+if($count == 0)
+{
+    // comment not found
+    echo " <script>alert('Comment not found.'); window.location.href='Events.php'</script>  ";
+}
+
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+if($row['username'] != $_SESSION["username"])
+{
+    // user is not correct
+    echo " <script>alert('Cannot edit comments you did not make.'); window.location.href='Events.php'</script>  ";
+}
+
+?>
+
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Registration</title>
+  <link rel="stylesheet" href="stylesheets/screen.css" type="text/css" media="screen" charset="utf-8">
+</head>
+
+<div id="page">
+  <div id="header">
+    <h1>User Login</h1>
+  </div>
+  
+  <div id="navigation">
+    <ul class="tabs">
+      <li> </li>
+    </ul>
+  </div>
+  
+  <h2>Original Description</h2>
+  <? php echo <td>".$row['comment_text']."</td> ?>
+  <?php
+    echo "<tr>
+           <td>".$row['comment_text']." </td>
+          </tr>";
+  ?>
+
+  <div id="body" class="wrapper">
+    <div id="introduction">
+      <h2>Edit Comment</h2>
+      <form action="modifyCommentDB.php" method="post" class="hform" name="mod" id="mod">
+        <fieldset>
+          <legend></legend>
+                Description: <input id="forminput" type="text" name="description" /><br><br>
+        </fieldset>
+        
+        <p>
+          <input type="submit" name="submit" value="Submit" class="button">
+          <input type="button" onclick="document.getElementById('signon').reset()" value="Clear">
+        </p>
+      </form>
+      <!--
+      <form action="Registration1.html">
+              <input type="submit" value="Register" />
+      </form>
+      !-->
+      
+    </div>
+    <div id="resources">
+    </div>
+  </div>
+  
+  <div id="footer">
+    <ul class="tabs">
+      <li></li>
+    </ul>
+    
+  </div>
+</div>
+
+</body>
+</html>
+
+
+
+?>
---
+++ HTML/modifyCommentDB.php
@@ -0,0 +1,31 @@
+<?php
+// Always start this first
+session_start();
+$db_host = 'localhost'; // Server Name
+$db_user = 'root'; // Username
+$db_pass = ''; // Password
+$db_name = 'event_creator'; // Database Name
+
+$_SESSION["LoggedIn"] = true;
+$_SESSION["Level"] = "admin";
+
+$user_level = $_SESSION['level'];
+
+$con = mysqli_connect("localhost", "root", "", "event_creator");
+mysqli_select_db($con, "comments");
+
+//$_POST['description']
+
+//$sql = "SELECT comment_text from comments";     
+$sql = "UPDATE comments
+		SET comment_text = '$_POST[description]'
+		WHERE username = '$_SESSION["username"]'
+			AND c_id = $_SESSION["commentID"]";     
+
+// modify database entry
+
+mysqli_query($con, $sql) or trigger_error("Query Failed! SQL: $sql - Error: ".mysqli_error($con), E_USER_ERROR);
+
+echo "<script> window.location.href = 'Events.php'</script>";
+
+?>
